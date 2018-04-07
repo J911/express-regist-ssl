a1592e1e59d5491e34ac169b5bb6c28e8c9bc45a
J911 <fb.jaemin@gmail.com>
2018-04-07 18:12:48 +0900

    initialize code

---
+++ index.js
@@ -0,0 +1,19 @@
+const https = require('https');
+const fs = require('fs');
+
+class express_ssl {
+    constructor(_key, _cert, _port = 443) {
+        this.key = fs.readFileSync(_key);
+        this.cert = fs.readFileSync(_cert);
+        this.port = _port;
+    }
+
+    createServer(_app, callback = null) {
+        https.createServer({
+            key: this.key,
+            cert: this.cert
+        }, _app).listen(this.port, callback);
+    }
+}
+
+module.exports = express_ssl;
